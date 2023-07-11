b971cc64f64020c2ab3c95dcd6730857fac8cf03
cbrantley318 <114195407+cbrantley318@users.noreply.github.com>
2023-07-12 01:18:51 +0200

    testing calendly integration

--- clarke-physio/src/App.js
+++ clarke-physio/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/common/NavBar";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { InlineWidget } from "react-calendly";
 
 function App() {
   return (
     <div>
-      
+      <NavBar></NavBar>
 
+      <div className="container">
+        <div className="card mt-4 mb-5">
+          <div className="card-body text-center p-3">Hello</div>
+        </div>
+          <div className="col-3 p-3">
+            <InlineWidget url="https://calendly.com/carson-brantley"></InlineWidget>
+          </div>
+      </div>
     </div>
   );
 }
---
+++ clarke-physio/src/components/common/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './NavBar.css';
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css"
+
+export default function NavBar() {
+  return (
+    <div className='bg-dark fixed-top Navbar p-2 justify-content'> Blank</div>
+  )
+}
