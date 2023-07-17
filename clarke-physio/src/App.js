cc102808e5a672d82c6777802499d2dc40501bf4
cbrantley318 <114195407+cbrantley318@users.noreply.github.com>
2023-07-17 12:25:42 +0200

    Updated home page to include AboutMe and Booking sections

--- clarke-physio/src/App.js
+++ clarke-physio/src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 import NavBar from "./components/common/NavBar";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
- import AboutPage from "./components/pages/AboutPage";
 import HomePage from "./components/pages/HomePage";
 import ServicesPage from "./components/pages/ServicesPage";
 import ContactPage from "./components/pages/ContactPage";
+import AboutMe from "./components/pages/AboutMe";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
-        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/about" element={<AboutMe/>}/>
         <Route path="/services" element={<ServicesPage/>}/>
         <Route path="/contact" element={<ContactPage/>}/>
       </Routes>
--- clarke-physio/src/components/pages/HomePage.js
+++ clarke-physio/src/components/pages/HomePage.js
@@ -1,30 +1,17 @@
 import React from "react";
 import { InlineWidget } from "react-calendly";
 import { SocialIcon } from "react-social-icons";
+import AboutMe from "./AboutMe";
+import Introduction from "./Introduction";
+import Booking from "./Booking";
 
 export default function HomePage() {
   return (
-    <div className="container-fluid bg-light ">
-      <div className="card mt-4 mb-5">
-        <div className="card-body text-center p-3 text-center">Home Page placeholder {window.innerWidth}</div>
-      </div>
-      <div className="row mb-5">
-        <div className="col-md-6 mt-2 border pt-3 ">
-          <InlineWidget url="https://calendly.com/carson-brantley"></InlineWidget>
-        </div>
+      <div className="container-fluid bg-light text-center">
+        <Introduction></Introduction>
+        <AboutMe></AboutMe>
+        <Booking></Booking>
 
-        <div className="col-md-6 mt-2 border-top border-end border-bottom pt-3 text-center">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.853810016068!2d18.3871124!3d-33.9191619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc67223a3a0967%3A0xb0e5e905989aa540!2s19%20Kloof%20Rd!5e0!3m2!1sen!2sza!4v1689165708794!5m2!1sen!2sza"
-            width= {window.innerWidth < 768 ? "90%": "90%"}
-            height= {window.innerWidth > 768 ? "90%": 500 }
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
       </div>
-    </div>
   );
 }
