39b18e61c85ee8be0366a0b3090b46dc39d13df6
Giovanni Smith <giovanni.smith@duke.edu>
2023-07-17 19:53:13 +0200

    bg color on cards changed

--- clarke-physio/src/components/common/NavBar.js
+++ clarke-physio/src/components/common/NavBar.js
@@ -27,21 +27,21 @@ export default function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto">
             <li className="nav-item active">
-              <Link className="nav-link text-light" to="/">
+              <Link className="nav-link text-light " to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item active">
+            <li className=" nav-item active ">
               <Link className="nav-link text-light" to="/about">
                 About
               </Link>
             </li>
-            <li className="nav-item active">
+            <li className=" nav-item active ">
               <Link className="nav-link text-light" to="/services">
                 Services
               </Link>
             </li>
-            <li className="nav-item active">
+            <li className=" nav-item active ">
               <Link className="nav-link text-light" to="/contact">
                 Contact Us
               </Link>
