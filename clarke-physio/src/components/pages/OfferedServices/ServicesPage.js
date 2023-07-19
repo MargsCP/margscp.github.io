a0448a5fb0b6cb763bade2b0b621597296636270
cbrantley318 <114195407+cbrantley318@users.noreply.github.com>
2023-07-19 15:31:07 +0200

    Fixed some merge issues

--- clarke-physio/src/components/pages/OfferedServices/ServicesPage.js
+++ clarke-physio/src/components/pages/OfferedServices/ServicesPage.js
@@ -7,10 +7,9 @@ export default function ServicesPage() {
   return (
     <div className="py-5 services-bg" id="services">
       <h1 className="text-center header">
-    // <div style={{backgroundImage: "linear-gradient(180deg, rgb(238, 235, 229), rgb(167, 154, 139))"}}>
         Our Services
       </h1>
-      <hr className="horizontal-line text-center mb-5" style={{color: "#623E29", width: "50%", borderRadius: 4}} ></hr>
+      <hr className="horizontal-line text-center mb-5"></hr>
       <div
         style={{
           marginLeft: "auto",
