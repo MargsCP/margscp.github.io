5387968ec0ec72f1ca12a3edf31dfee429755f6a
cbrantley318 <114195407+cbrantley318@users.noreply.github.com>
2023-07-21 14:01:43 +0200

    Formatted styles.css file

--- clarke-physio/src/App.js
+++ clarke-physio/src/App.js
@@ -4,8 +4,6 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
-import ServicesPage from "./components/pages/OfferedServices/ServicesPage";
-import ContactPage from "./components/pages/ContactPage";
 
 import Footer from "./components/pages/Footer";
 import AboutPage from "./components/pages/AboutPage";
@@ -17,8 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage/>} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
       </Routes>
       {/* <Footer></Footer> */}
     </BrowserRouter>
