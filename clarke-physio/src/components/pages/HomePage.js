e5a3f9d9847041ec776d3f4ae4dec09606669564
cbrantley318 <114195407+cbrantley318@users.noreply.github.com>
2023-07-24 15:36:46 -0400

    Updated Documentation

--- clarke-physio/src/components/pages/HomePage.js
+++ clarke-physio/src/components/pages/HomePage.js
@@ -13,6 +13,12 @@ export default function HomePage() {
   /* This section is fairly straightforward, just contains every fold on the home page
    There is an alternative photograph fold imported that has two photos instead of one 
    in case you wish to replace it or to add a second photo fold
+
+   The testimonials sections was commented out, once actual clients have provided some testimonials, uncomment it out and feel free to readjust the order of the fold on the website
+
+   If any images appear to be stretched abnormally that we've missed, add in an "objectFit: cover" attribute to the image.
+   Please see either the #title-img in styles.css or the first image in AboutPage.js file as reference
+
   
   */
 
@@ -25,7 +31,7 @@ export default function HomePage() {
         <PhotoSection/>
         <ServicesPage/>
         <Booking/>
-        <Testimonials/>
+        {/* <Testimonials/> */}
       </div>
     </>
   );
